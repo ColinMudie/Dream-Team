d7e6e0fe17a20315a8fc2e208faceea8952d1561
Sara Taylor <sara.swifti@gmail.com>
2021-05-01 13:32:57 -0400

    identigying each piano key and keyvalue pairs in object

--- dreamteamClient/src/utils/KeysObj.js
+++ dreamteamClient/src/utils/KeysObj.js
@@ -7,10 +7,83 @@
 const KeysObj = [
     {
         id:1,
-        keyname: "C",
-        keytype: "white or black placeholder",
+        keyname: "c",
+        keytype: "white-lefthand placeholder",
         keypitch: "pathnameplaceholder"
     },    
+    {
+        id:2,
+        keyname: "c#",
+        keytype: "black placeholder",
+        keypitch: "pathnameplaceholder"
+    },
+    {
+        id:3,
+        keyname: "d",
+        keytype: "white-center placeholder",
+        keypitch: "pathnameplaceholder"
+    },
+    {
+        id:4,
+        keyname: "d#",
+        keytype: "black placeholder",
+        keypitch: "pathnameplaceholder"
+    },
+    {
+        id:5,
+        keyname: "e",
+        keytype: "white-righthand placeholder",
+        keypitch: "pathnameplaceholder"
+    },
+    {
+        id:6,
+        keyname: "f",
+        keytype: "white-lefthand placeholder",
+        keypitch: "pathnameplaceholder"
+    },
+    {
+        id:7,
+        keyname: "f#",
+        keytype: "black placeholder",
+        keypitch: "pathnameplaceholder"
+    },
+    {
+        id:8,
+        keyname: "g",
+        keytype: "white-center placeholder",
+        keypitch: "pathnameplaceholder"
+    },
+    {
+        id:9,
+        keyname: "g#",
+        keytype: "black placeholder",
+        keypitch: "pathnameplaceholder"
+    },
+    {
+        id:10,
+        keyname: "a",
+        keytype: "white-center placeholder",
+        keypitch: "pathnameplaceholder"
+    },
+    {
+        id:11,
+        keyname: "a#",
+        keytype: "black placeholder",
+        keypitch: "pathnameplaceholder"
+    },
+    {
+        id:12,
+        keyname: "b",
+        keytype: "white-righthand placeholder",
+        keypitch: "pathnameplaceholder"
+    },
+    {
+        id:13,
+        keyname: "c2",
+        keytype: "white-lefthand placeholder",
+        keypitch: "pathnameplaceholder"
+    },
+    
 ]
 
 export default KeysObj;
