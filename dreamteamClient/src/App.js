5881eb84fc9d217de9e2c02aafcc040185db1ea7
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-05-03 13:43:15 -0400

    kayboard is complete

--- dreamteamClient/src/App.js
+++ dreamteamClient/src/App.js
@@ -1,16 +1,12 @@
 import React from "react";
 // import Key from "./Components/Key/Key";
 // import Keyboard from "./components/Keyboard/Keyboard";
-import WhiteRow from "./Components/WhiteRow/WhiteRow";
-import BlackRow from "./Components/BlackRow/BlackRow";
-import BlackRow2 from "./Components/BlackRow/BlackRow2";
+import Keyboard from "../src/Components/Keyboard/Keyboard";
 
 function App() {
     return (
         <>
-        <WhiteRow />
-        <BlackRow />
-        <BlackRow2 />
+        <Keyboard />
         </>
     );
 }
