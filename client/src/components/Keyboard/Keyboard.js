c665c6a59a5dd2580eea15723c05bde07b27a208
ColinMudie <csmudie1@gmail.com>
2021-05-12 20:27:47 -0400

    Revert "added keyboard events to trigger the synth"

--- client/src/components/Keyboard/Keyboard.js
+++ client/src/components/Keyboard/Keyboard.js
@@ -1,81 +1,20 @@
-import React, {useEffect, useContext} from 'react';
-import SynthContext from '../../utils/SynthContext';
+import React from 'react';
 import BlackRow from '../BlackRow/BlackRow';
-import Oscillator from '../../utils/oscillator';
 import WhiteRow from '../WhiteRow/WhiteRow';
 // import Container from '@material-ui/core/Container';
-import whiteObj from '../../utils/whiteObj';
-import blackObj from '../../utils/blackObj';
-import blackObj2 from '../../utils/blackObj2';
 import Grid from "@material-ui/core/Grid";
 import './Keyboard.css';
 
 function Keyboard() {
-    const { attack, decay, filter, volume } = useContext(SynthContext);
-    useEffect(() => {
-        function keyOscillator(object) {
-            Oscillator(object.frequency, attack, decay, filter, volume)
-        }
-        const handleKeyPress = (event) => {
-            switch (event.key) {
-                case 'a':
-                    keyOscillator(whiteObj[0])
-                    break;
-                case 's':
-                    keyOscillator(whiteObj[1])
-                    break;
-                case 'd':
-                    keyOscillator(whiteObj[2])
-                    break;
-                case 'f':
-                    keyOscillator(whiteObj[3])
-                    break;
-                case 'g':
-                    keyOscillator(whiteObj[4])
-                    break;
-                case 'h':
-                    keyOscillator(whiteObj[5])
-                    break;
-                case 'j':
-                    keyOscillator(whiteObj[6])
-                    break;
-                case 'k':
-                    keyOscillator(whiteObj[7])
-                    break;
-                case 'w':
-                    keyOscillator(blackObj[0])
-                    break;
-                case 'e':
-                    keyOscillator(blackObj[1])
-                    break;
-                case 't':
-                    keyOscillator(blackObj2[0])
-                    break;
-                case 'y':
-                    keyOscillator(blackObj2[1])
-                    break;
-                case 'uas':
-                    keyOscillator(blackObj2[2])
-                    break;
-                default:
-                    break;
-            }
-        }
-        window.addEventListener('keydown', handleKeyPress);
-
-        return () => {
-            window.removeEventListener('keydown', handleKeyPress);
-        };
-    }, [attack, decay, filter, volume]);
     return (
-        
+       
             <Grid>
                 <div className="keyboard">
                     <BlackRow/>
                     <WhiteRow />
                 </div>
             </Grid>
-        
+       
     );
 }
 
--- client/src/components/WhiteKey/WhiteKey.js
+++ client/src/components/WhiteKey/WhiteKey.js
@@ -1,27 +1,15 @@
 import React, { useContext } from 'react';
 import Oscillator from '../../utils/oscillator';
 import SynthContext from '../../utils/SynthContext';
-import ReactDOM from 'react-dom';
-
 
 const WhiteKey = ({ whiteKey }) => {
     const { attack, decay, filter, volume } = useContext(SynthContext);
-    function handleOnClick (item) {
-        Promise.resolve().then(() => {
-            ReactDOM.unstable_batchedUpdates(() => {
-                Oscillator(item.frequency, attack, decay, filter, volume)
-            })
-        })
-    }
 
     return (
         <div className="white-key">
             {
                 whiteKey.map((item) => {
-                    return <div 
-                    className="w-key" 
-                    key={item.id} 
-                    onClick={() => handleOnClick(item)}>
+                    return <div className="w-key" key={item.id} onClick={() => Oscillator(item.frequency, attack, decay, filter, volume)}>
                         <h5>
                             <span>{item.keyname}</span>
                         </h5>
