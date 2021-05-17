51cd3a1041be14676e1014d46235b88afa3bcd1f
Sara Taylor <sara.swifti@gmail.com>
2021-05-17 01:13:28 -0400

    saving and walking away to look at later

--- client/src/components/CheckBoxWave/CheckBoxWave.js
+++ client/src/components/CheckBoxWave/CheckBoxWave.js
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import "./CheckBoxWave.css";
 // import ReactDom from "react-dom";
 
 
-function CheckBoxWave ({ name, checked, onUpdate }) {
+function CheckBoxWave ({ checked, onUpdate }) {
 return (
       <div className="grid">
       <Grid container className="checkboxcontainer" xs={12} spacing={1} direction="row" justify="flex-end" alignItmes="flex-end">
@@ -14,24 +14,24 @@ return (
         </Typography>
 
         <Grid item xs={1} direction="column">
-          <input type="checkbox" name="sine" value={1} checked={checked === 1} onChaged={e => onUpdate({ name, checked: +e.target.value })} />
+          <input type="checkbox" name="sine" value={1} checked={checked === 1} onChange={e => onUpdate({ checked: +e.target.value })} />
           <br />
           <label for="Sine"> Sine</label>
         </Grid>
         <Grid item xs={1} direction="column">
-          <input type="checkbox" name="sawtooth" value={2} checked={checked === 2} onChaged={e => onUpdate({ name, checked: +e.target.value })} />
+          <input type="checkbox" name="sawtooth" value={2} checked={checked === 2} onChange={e => onUpdate({ checked: +e.target.value })} />
           <br />
           <label for="Sawtooth"> Sawtooth</label>
 
         </Grid>
         <Grid item xs={1} direction="column">
-          <input type="checkbox" name="square" value={3} checked={checked === 3} onChaged={e => onUpdate({ name, checked: +e.target.value })} />
+          <input type="checkbox" name="square" value={3} checked={checked === 3} onChange={e => onUpdate({ checked: +e.target.value })} />
           <br />
           <label for="Square"> Square</label>
 
         </Grid>
         <Grid item xs={1} direction="column">
-          <input type="checkbox" name="triangle" value={4} checked={checked === 4} onChaged={e => onUpdate({ name, checked: +e.target.value })} />
+          <input type="checkbox" name="Triangle" value={4} checked={checked === 4} onChange={e => onUpdate({ checked: +e.target.value })} />
           <br />
           <label for="Triangle"> Triangle</label>
 
@@ -43,44 +43,6 @@ return (
     )
   }
  
-
-
-//the below property is what is rendering my original html return
-  // return (
-
-  //   <div className="grid">
-  //     <Grid container className="checkboxcontainer" xs={12} spacing={1} direction="row" justify="flex-end" alignItmes="flex-end">
-  //       <Typography item direction="column"  justify="flex-end" className="waveheader" xs={5}>
-  //         <h3>Wave</h3>
-  //       </Typography>
-
-  //       <Grid item xs={1} direction="column">
-  //         <input type="checkbox" class="radio" value="1" name="fooby[1][]" id={props.sine} />
-  //         <br />
-  //         <label for="Sine"> Sine</label>
-  //       </Grid>
-  //       <Grid item xs={1} direction="column">
-  //         <input type="checkbox" class="radio" value="1" name="fooby[1][]" id={props.sawtooth} />
-  //         <br />
-  //         <label for="Sawtooth"> Sawtooth</label>
-
-  //       </Grid>
-  //       <Grid item xs={1} direction="column">
-  //         <input type="checkbox" class="radio" value="1" name="fooby[1][]" id={props.square} />
-  //         <br />
-  //         <label for="Square"> Square</label>
-
-  //       </Grid>
-  //       <Grid item xs={1} direction="column">
-  //         <input type="checkbox" class="radio" value="1" name="fooby[1][]" id={props.triangle} />
-  //         <br />
-  //         <label for="Triangle"> Triangle</label>
-
-  //       </Grid>
-  //     </Grid>
-
-  //   </div>
-  // );
 
 
 export default CheckBoxWave;
