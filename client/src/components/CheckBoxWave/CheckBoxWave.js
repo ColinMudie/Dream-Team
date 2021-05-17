758792421277a709bbe51ac814233d0c853fa921
Sara Taylor <sara.swifti@gmail.com>
2021-05-17 00:26:01 -0400

    added js to get only one box to render at a time

--- client/src/components/CheckBoxWave/CheckBoxWave.js
+++ client/src/components/CheckBoxWave/CheckBoxWave.js
@@ -1,38 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import "./CheckBoxWave.css";
+// import ReactDom from "react-dom";
 
 
-const CheckBoxWave = (props) => {
+class CheckBoxWave extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
+    this.state = {
+      selectedId: null,
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-    <div className="grid">
+  handleChange(id, value) {
+    this.setState({selectedId: value===true?id:null})
+  }
+  click(value) {
+    this.props.handleChangess(this.state.fields.id, value);
+  };
+
+  selectOnlyThis(){
+
+  }
+  
+  render(props) {
+    return (
+      <div className="grid">
       <Grid container className="checkboxcontainer" xs={12} spacing={1} direction="row" justify="flex-end" alignItmes="flex-end">
         <Typography item direction="column"  justify="flex-end" className="waveheader" xs={5}>
           <h3>Wave</h3>
         </Typography>
 
         <Grid item xs={1} direction="column">
-          <input type="checkbox" id={props.sine} />
+          <input type="checkbox" class="radio" value="1" selectedId={this.state.selectedId} id="1" />
           <br />
           <label for="Sine"> Sine</label>
         </Grid>
         <Grid item xs={1} direction="column">
-          <input type="checkbox" id={props.sawtooth} />
+          <input type="checkbox" class="radio" value="1" selectedId={this.state.selectedId} id="2" />
           <br />
           <label for="Sawtooth"> Sawtooth</label>
 
         </Grid>
         <Grid item xs={1} direction="column">
-          <input type="checkbox" id={props.square} />
+          <input type="checkbox" class="radio" value="1" selectedId={this.state.selectedId} id="3" />
           <br />
           <label for="Square"> Square</label>
 
         </Grid>
         <Grid item xs={1} direction="column">
-          <input type="checkbox" id={props.triangle} />
+          <input type="checkbox" class="radio" value="1" selectedId={this.state.selectedId} id="4" />
           <br />
           <label for="Triangle"> Triangle</label>
 
@@ -40,7 +60,48 @@ const CheckBoxWave = (props) => {
       </Grid>
 
     </div>
-  );
+
+    )
+  }
+ 
+
+
+//the below property is what is rendering my original html return
+  // return (
+
+  //   <div className="grid">
+  //     <Grid container className="checkboxcontainer" xs={12} spacing={1} direction="row" justify="flex-end" alignItmes="flex-end">
+  //       <Typography item direction="column"  justify="flex-end" className="waveheader" xs={5}>
+  //         <h3>Wave</h3>
+  //       </Typography>
+
+  //       <Grid item xs={1} direction="column">
+  //         <input type="checkbox" class="radio" value="1" name="fooby[1][]" id={props.sine} />
+  //         <br />
+  //         <label for="Sine"> Sine</label>
+  //       </Grid>
+  //       <Grid item xs={1} direction="column">
+  //         <input type="checkbox" class="radio" value="1" name="fooby[1][]" id={props.sawtooth} />
+  //         <br />
+  //         <label for="Sawtooth"> Sawtooth</label>
+
+  //       </Grid>
+  //       <Grid item xs={1} direction="column">
+  //         <input type="checkbox" class="radio" value="1" name="fooby[1][]" id={props.square} />
+  //         <br />
+  //         <label for="Square"> Square</label>
+
+  //       </Grid>
+  //       <Grid item xs={1} direction="column">
+  //         <input type="checkbox" class="radio" value="1" name="fooby[1][]" id={props.triangle} />
+  //         <br />
+  //         <label for="Triangle"> Triangle</label>
+
+  //       </Grid>
+  //     </Grid>
+
+  //   </div>
+  // );
 }
 
 export default CheckBoxWave;
