6fe297cdac724e1a7bb46d1a85b4c0ad7534ef46
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-05-20 13:01:42 -0400

    new keyboard up and running

--- client/src/components/Keyboard/Keyboard2.js
+++ client/src/components/Keyboard/Keyboard2.js
@@ -1,71 +1,137 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Oscillator from '../../utils/oscillator';
 import SynthContext from '../../utils/SynthContext';
-import KeypressContext from '../../utils/keypressContext';
+import keypressContext from '../../utils/keypressContext';
 import Grid from '@material-ui/core/Grid';
 import './Keyboard 2.css';
-import { useState } from 'react';
 import keysObj from '../../utils/keysObj';
 
 const Keyboard2 = () => {
-    const { activeKey } = useContext(KeypressContext);
-    const { parameters } = useContext(SynthContext);
+    
     const [keysInfo] = useState(keysObj)
+
+    const [activeKey, setActiveKey] = useState(-1);
+    const { parameters } = useContext(SynthContext);
+    
+    useEffect(() => {
+        const resetActiveKey = () => {
+            setActiveKey(-1)
+            console.log(activeKey);
+        }
+        const handleKeyPress = (event) => {
+            function keyOscillator(object, sustain) {
+               Oscillator(object.frequency, parameters.attack, parameters.decay, parameters.filter, parameters.waveShape, parameters.volume, sustain)
+            }
+            switch (event.key) {
+                case 'a':
+                    setActiveKey(6);
+                    console.log(activeKey)
+                    keyOscillator(keysObj[2][0], activeKey)                    
+                    break;
+                case 's':
+                    keyOscillator(keysObj[2][1])
+                    setActiveKey(7)
+                    break;
+                case 'd':
+                    keyOscillator(keysObj[2][2])
+                    setActiveKey(8)
+                    break;
+                case 'f':
+                    keyOscillator(keysObj[2][3])
+                    setActiveKey(9)
+                    break;
+                case 'g':
+                    keyOscillator(keysObj[2][4])
+                    setActiveKey(10)
+                    break;
+                case 'h':
+                    keyOscillator(keysObj[2][5])
+                    setActiveKey(11)
+                    break;
+                case 'j':
+                    keyOscillator(keysObj[2][6])
+                    setActiveKey(12)
+                    break;
+                case 'k':
+                    keyOscillator(keysObj[2][7])
+                    setActiveKey(13)
+                    break;
+                case 'w':
+                    keyOscillator(keysObj[0][0])
+                    setActiveKey(1)
+                    break;
+                case 'e':
+                    keyOscillator(keysObj[0][1])
+                    setActiveKey(2)
+                    break;
+                case 't':
+                    keyOscillator(keysObj[1][0])
+                    setActiveKey(3)
+                    break;
+                case 'y':
+                    keyOscillator(keysObj[1][1])
+                    setActiveKey(4)
+                    break;
+                case 'u':
+                    keyOscillator(keysObj[1][2])
+                    setActiveKey(5)
+                    break;
+                default:
+                    break;
+            }
+        }
+        window.addEventListener('keydown', handleKeyPress);
+        window.addEventListener('keyup', resetActiveKey);
+        return () => {
+            window.removeEventListener('keydown', handleKeyPress);
+            window.removeEventListener('keydown', resetActiveKey);
+        };
+    }, [parameters.attack, parameters.decay, parameters.filter, parameters.waveShape, parameters.volume, activeKey]);
     return (
-        <>
-        {
-            keysInfo.map(item => (
+        <keypressContext.Provider value={{ activeKey, setActiveKey }}>
+
             <Grid container className="keyboard-row" xs={20} direction="row">
 
                 <Grid sx={{ flexGrow: 1 }} className="black-key-row-left">
-                    
-                    <Grid container  xs={16} direction="row">
-                        <div className={`bl-key ${activeKey === item.id ? "active" : ""}`}
-                        key={item.id} onClick={() => Oscillator(item.frequency, parameters.attack, parameters.decay, parameters.filter, parameters.waveShape, parameters.volume)} >
-                 <h5 className="bl-note">
-                    <span>{item.keyname}</span>
-                </h5>
-                        </div>
-                        <div  className="bl-key" >
-                        </div>
+
+                    <Grid container xs={16} direction="row">
+                        {
+                            keysInfo[0].map(item => {
+                                return <div 
+                                onClick={() => Oscillator(item.frequency, parameters.attack, parameters.decay, parameters.filter, parameters.waveShape, parameters.volume)}
+                                 className= {`bl-key ${activeKey === item.id ? "active" : ""}`}>
+                                </div>
+                            })}
                     </Grid>
                 </Grid>
 
                 <Grid sx={{ flexGrow: 1 }} className="black-key-row-right">
-                    <Grid container  xs={16} direction="row">
-                        <div  className="bl-key" >
-                        </div>
-                        <div  className="bl-key" >
-                        </div>
-                        <div  className="bl-key" >
-                        </div>
+                    <Grid container xs={16} direction="row">
+                    {
+                            keysInfo[1].map(item => {
+                                return <div 
+                                onClick={() => Oscillator(item.frequency, parameters.attack, parameters.decay, parameters.filter, parameters.waveShape, parameters.volume)}
+                                 className= {`bl-key ${activeKey === item.id ? "active" : ""}`}>
+                                </div>
+                            })}
                     </Grid>
                 </Grid>
 
                 <Grid sx={{ flexGrow: 1 }} className="white-key-row">
-                    <Grid container  xs={16} direction="row">
-                        <div  className="wh-key" >
-                        </div>
-                        <div  className="wh-key" >
-                        </div>
-                        <div  className="wh-key" >
-                        </div>
-                        <div  className="wh-key" >
-                        </div>
-                        <div  className="wh-key" >
-                        </div>
-                        <div  className="wh-key" >
-                        </div>
-                        <div  className="wh-key" >
-                        </div>
-                        <div  className="wh-key" >
-                        </div>
+                    <Grid container xs={16} direction="row">
+                    {
+                            keysInfo[2].map(item => {
+                                return <div 
+                                onClick={() => Oscillator(item.frequency, parameters.attack, parameters.decay, parameters.filter, parameters.waveShape, parameters.volume)}
+                                 className= {`wh-key ${activeKey === item.id ? "active" : ""}`}>
+                                </div>
+                            })}
                     </Grid>
                 </Grid>
             </Grid>
-            ))
-}
-        </>
+
+
+            </keypressContext.Provider>
     );
 }
 
--- client/src/utils/keysObj.js
+++ client/src/utils/keysObj.js
@@ -1,70 +1,77 @@
 const key = [
-    {
-        id:1,
-        keyname: "C♯/D♭",
-        frequency: 554
-    },    
-    {
-        id:2,
-        keyname: "D♯/E♭",
-        frequency: 622
-    },
-    {
-        id: 3,
-        keyname: "F♯/G♭",
-        frequency: 740
-    },
-    {
-        id: 4,
-        keyname: "G♯/A♭",
-        frequency: 831
-    },
-    {
-        id: 5,
-        keyname: "A♯/B♭",
-        frequency: 932
-    },
-    {
-        id: 6,
-        keyname: "C",
-        frequency: 523
-    },
-    {
-        id: 7,
-        keyname: "D",
-        frequency: 587
-    },
-    {
-        id: 8,
-        keyname: "E",
-        frequency: 659
-    },
-    {
-        id: 9,
-        keyname: "F",
-        frequency: 699
-    },
-    {
-        id: 10,
-        keyname: "G",
-        frequency: 784
-    },
-    {
-        id: 11,
-        keyname: "A",
-        frequency: 880
-    },
-    {
-        id: 12,
-        keyname: "B",
-        frequency: 988
-    },
-    {
-        id: 13,
-        keyname: "C",
-        frequency: 1047
-    }
-
+    [
+        {
+            id: 1,
+            keyname: "C♯/D♭",
+            frequency: 554,
+            type: "black"
+        },
+        {
+            id: 2,
+            keyname: "D♯/E♭",
+            frequency: 622,
+            type: "black"
+        }
+    ],
+    [
+        {
+            id: 3,
+            keyname: "F♯/G♭",
+            frequency: 740
+        },
+        {
+            id: 4,
+            keyname: "G♯/A♭",
+            frequency: 831
+        },
+        {
+            id: 5,
+            keyname: "A♯/B♭",
+            frequency: 932
+        }
+    ],
+    [
+        {
+            id: 6,
+            keyname: "C",
+            frequency: 523
+        },
+        {
+            id: 7,
+            keyname: "D",
+            frequency: 587
+        },
+        {
+            id: 8,
+            keyname: "E",
+            frequency: 659
+        },
+        {
+            id: 9,
+            keyname: "F",
+            frequency: 699
+        },
+        {
+            id: 10,
+            keyname: "G",
+            frequency: 784
+        },
+        {
+            id: 11,
+            keyname: "A",
+            frequency: 880
+        },
+        {
+            id: 12,
+            keyname: "B",
+            frequency: 988
+        },
+        {
+            id: 13,
+            keyname: "C",
+            frequency: 1047
+        }
+    ]
 ]
 
 export default key;
